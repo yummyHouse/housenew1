1d79b5e1a94c6736a1231b54eae355ad78819fa5
okhouse <house0986993019@gmail.com>
2023-05-10 14:55:39 +0800

    新增規格表

--- scripts/header.js
+++ scripts/header.js
@@ -1,14 +1,15 @@
-header =`<nav class="header">
+header = `<nav class="header">
 <ul class="menu">
 <li><a href="index.html">總表</a></li>
 <li><a href="basic.html">基礎樣式</a></li>
 <li><a href="table.html">表格樣式</a></li>
 <li><a href="block.html">區塊樣式</a></li>
+<li><a href="Specification.html">規格表</a></li>
 </ul>
 <p id="show">
 你複製了什麼?
 </p>
 </nav>
 <a href="debug.html"><div class="protect">保護眼睛計畫</div></a>
-`;
-document.write(header);
+`
+document.write(header)
